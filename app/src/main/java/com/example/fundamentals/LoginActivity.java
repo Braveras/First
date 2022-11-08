76e9f37230165b930432070c41001ac045e89ee0
alejandro bravo <acunamatata1997@gmail.com>
2022-11-08 13:58:27 +0100

    Initial commit after new start

---
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.fundamentals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    // Method for Signup text
+    public void openSignup(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+}
