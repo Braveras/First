9eb69139dea33bc0b2a60443748f58581faf15c6
alejandro bravo <acunamatata1997@gmail.com>
2022-11-10 10:47:20 +0100

    Initial commit

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -3,15 +3,49 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.DrawableContainer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageView innet;
+    private ImageView logito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        innet = findViewById(R.id.girl);
+        logito = findViewById(R.id.tree);
+
+        //String imagen = "https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80";
+        //Glide.with(getApplicationContext()).load(imagen).into(innet);
+
+
+        Glide.with(this)
+//              .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .placeholder(R.drawable.gradient)
+                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
+//                .circleCrop()
+                .into(innet);
+
+        Glide.with(this)
+//              .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .load(R.drawable.pepotrump)
+//                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
+                .circleCrop()
+                .into(logito);
     }
 
     // Method for Signup text
