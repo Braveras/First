5b1cfd092d76602e832038aa240770aa915a1ac5
Braveras <acunamatata1997@gmail.com>
2022-11-28 20:53:58 +0100

    Finishing signup activity with new styles

--- app/src/main/java/com/example/fundamentals/SignupActivity.java
+++ app/src/main/java/com/example/fundamentals/SignupActivity.java
@@ -3,12 +3,49 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText edituser, editpassword, editpassword2, editemail;
+    Button cancel;
+    ImageView mFondo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        cancel = findViewById(R.id.cancel);
+        edituser = findViewById(R.id.edituser);
+        editpassword = findViewById(R.id.editpassword);
+        editemail = findViewById(R.id.editemail);
+        editpassword2 = findViewById(R.id.editpassword2);
+        mFondo = findViewById(R.id.fondo);
+
+        Glide.with(this)
+                .load("https://cdn.discordapp.com/attachments/771931863102652417/1046875852127862834/unknown.png")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .into(mFondo);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edituser.setText("");
+                editpassword.setText("");
+                editpassword2.setText("");
+                editemail.setText("");
+
+            }
+        });
+
     }
 }
